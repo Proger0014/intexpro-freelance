40bf9ef5b2e29bb2ac5e275e78a87d1549c2908c
nikitalobodin <116456918+nikitalobodin@users.noreply.github.com>
2024-01-18 11:38:49 +0600

    убрал некоторые ненужные дефолтные вещи

--- app/Api/Http/Kernel.php
+++ app/Api/Http/Kernel.php
@@ -58,7 +58,6 @@ class Kernel extends HttpKernel
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Api\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \App\Api\Http\Middleware\ValidateSignature::class,
