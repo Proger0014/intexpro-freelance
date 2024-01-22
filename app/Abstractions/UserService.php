9b941a9a49d6216cb84d19272b67e12969d87e4a
proger0014 <iakov56@yandex.ru>
2024-01-22 13:11:57 +0600

    обновил user service интерфейс

--- app/Abstractions/UserService.php
+++ app/Abstractions/UserService.php
@@ -3,7 +3,47 @@
 namespace App\Abstractions;
 
 use App\Utils\Result;
+use DateTime;
 
 interface UserService {
-    function addNewUser(): Result;
+    /**
+     * @param array{
+     *  first_name: string,
+     *  last_name: string,
+     *  surname: string,
+     *  login: string,
+     *  date_of_birth: DateTime,
+     *  rating: float
+     * } $newUser
+     * 
+     * @param string $password
+     * 
+     * @return Result
+     */
+    function addNewUser(array $newUser, string $password): Result;
+
+    function getById(int $id): Result;
+
+    function getAll(): Result;
+
+    function getAllInPage(int $page): Result;
+
+    function getRolesOfUser(int $userId): Result;
+
+    function deleteByUserId(int $userId): Result;
+
+    /**
+     * @param array{
+     *  id: int,
+     *  first_name: string|null,
+     *  last_name: string|null,
+     *  surname: string|null,
+     *  login: string|null,
+     *  date_of_birth: DateTime|null,
+     *  rating: float|null
+     * } $updatedUser
+     */
+    function updateUser(array $updatedUser): Result;
+
+    function getByLogin(string $login): Result;
 }
