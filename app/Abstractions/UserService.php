d9440de1454bce9cdce72be535d9bbbdc7c52827
proger0014 <iakov56@yandex.ru>
2024-01-23 12:51:52 +0600

    Обновил UserService, generic указал загрушки. Буду реализовывать через таску

--- app/Abstractions/UserService.php
+++ app/Abstractions/UserService.php
@@ -11,28 +11,62 @@ interface UserService {
      * @param UserDto $newUser
      * @param string $password
      * 
-     * @return Result
+     * @return Result<bool>
      */
     function addNewUser(UserDto $newUser, string $password): Result;
 
+    /**
+     * @param int $id
+     * 
+     * @return Result<null>
+     */
     function getById(int $id): Result;
 
+    /**
+     * @return Result<null>
+     */
     function getAll(): Result;
 
+    /**
+     * @param int $page
+     * 
+     * @return Result<null>
+     */
     function getAllInPage(int $page): Result;
 
+    /**
+     * @param int $userId
+     * 
+     * @return Result<null>
+     */
     function getRolesOfUser(int $userId): Result;
 
+    /**
+     * @param int $userId
+     * 
+     * @return Result<null>
+     */
     function deleteByUserId(int $userId): Result;
 
     /**
      * @param UserDto $updatedUser
      * 
-     * @return Result
+     * @return Result<null>
      */
     function updateUser(UserDto $updatedUser): Result;
 
+    /**
+     * @param string $login
+     * 
+     * @return Result<null>
+     */
     function getByLogin(string $login): Result;
 
+    /**
+     * @param int $userId
+     * @param string $password
+     * 
+     * @return Result<null>
+     */
     function updatePasswordForUser(int $userId, string $newPassword): Result;
 }
