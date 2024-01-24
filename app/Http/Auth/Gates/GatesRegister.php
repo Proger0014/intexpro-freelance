07607f74351dcb8dfb363f4d37eab4be4975c3b7
proger0014 <iakov56@yandex.ru>
2024-01-24 14:04:57 +0600

    Начало для авторизации по ролям

--- app/Http/Auth/Gates/GatesRegister.php
+++ app/Http/Auth/Gates/GatesRegister.php
@@ -3,16 +3,15 @@
 namespace App\Http\Auth\Gates;
 
 use App\Abstractions\UserService;
-use Illuminate\Support\Facades\Auth;
 
 final class GatesRegister {
     public function __construct(
         public UserService $userService
     ) { }
 
+    /**
+     * TODO
+     */
     public function defineGatesForRole(): void {
-        $authenticatedUserId = Auth::user()->id;
-
-        $rolesResult = $this->userService->getRolesOfUser($authenticatedUserId);
     }
 }
