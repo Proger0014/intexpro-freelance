60e3ec0429124b05fc9e5d40e2995d3475aec110
proger0014 <iakov56@yandex.ru>
2024-01-24 20:11:16 +0600

    Добавил RoleService интерфейс; Добавил Role ресурсы

---
+++ app/Http/Resources/Role/RoleResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Role;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Role
+ */
+class RoleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
