6591ca725f102b6eebd3a5441fe795dcaa6be2e1
proger0014 <iakov56@yandex.ru>
2024-01-24 20:35:52 +0600

    Добавил RoleService в DI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Abstractions\RoleService;
 use App\Abstractions\UserService;
+use App\Services\RoleServiceImpl;
 use App\Services\UserServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(UserService::class, UserServiceImpl::class);
+        $this->app->singleton(RoleService::class, RoleServiceImpl::class);
     }
 
     /**
