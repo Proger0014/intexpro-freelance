6253092f8cd8b7ecb8f1c1bfda63681bf1ff2b3f
proger0014 <iakov56@yandex.ru>
2024-01-24 11:08:31 +0600

    Обновил UserCollectionResource

--- app/Http/Resources/User/UserCollectionResource.php
+++ app/Http/Resources/User/UserCollectionResource.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Database\Eloquent\Collection;
 use App\Models\User;
+use App\Dto\User\UserDto;
+use DateTimeImmutable;
 
 /**
  * @mixin Collection<int, User>
@@ -23,5 +25,21 @@ class UserCollectionResource extends ResourceCollection
             'count' => $this->collection->count(),
             'data' => UserResource::collection($this->collection)
         ];
+    }
+
+    /**
+     * @return array<UserDto>
+     */
+    public function toDtoArray(): array {
+        return $this->map(fn (User $user) =>
+            new UserDto(
+                $user->id,
+                $user->first_name,
+                $user->last_name,
+                $user->surname,
+                $user->login,
+                new DateTimeImmutable($user->date_of_birth),
+                $user->rating
+            ))->values()->toArray();
     }
 }
