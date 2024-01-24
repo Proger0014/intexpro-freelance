d0ee24dd864bf3d46defa02dac58c791221ae1e2
proger0014 <iakov56@yandex.ru>
2024-01-24 22:13:53 +0600

    Небольшой фикс

--- app/Http/Auth/Gates/GatesRegister.php
+++ app/Http/Auth/Gates/GatesRegister.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Auth\Gates;
 
+use App\Abstractions\RoleService;
 use App\Abstractions\UserService;
+use App\Exceptions\ErrorException;
 
 final class GatesRegister {
     public function __construct(
-        public UserService $userService
+        public readonly UserService $userService,
+        public readonly RoleService $roleService
     ) { }
 
     /**
-     * TODO
+     * @throws ErrorException
      */
     public function defineGatesForRole(): void {
+        $rolesResult = $this->roleService->getAll();
+
+        if ($rolesResult->isError()) {
+            $error = $rolesResult->getError();
+
+            throw new ErrorException(
+                type: $error->type,
+                title: $error->title,
+                status: $error->status,
+                detail: $error->detail
+            );
+        }
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,12 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use App\Abstractions\RoleService;
 use App\Exceptions\ErrorException;
 use App\Http\Auth\Gates\GatesRegister;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,19 +23,8 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @throws ErrorException
      */
-    public function boot(RoleService $roleService, GatesRegister $gatesRegister): void
+    public function boot(GatesRegister $gatesRegister): void
     {
-        $rolesResult = $roleService->getAll();
-
-        if ($rolesResult->isError()) {
-            $error = $rolesResult->getError();
-
-            throw new ErrorException(
-                type: $error->type,
-                title: $error->title,
-                status: $error->status,
-                detail: $error->detail
-            );
-        }
+        $gatesRegister->defineGatesForRole();
     }
 }
