7cca2ec6cd0515279c7b96fea691d18b4eb875bb
proger0014 <iakov56@yandex.ru>
2024-01-25 10:18:06 +0600

    Обновил login ошибку

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Responses\Error\ErrorResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $loginRequest): JsonResponse {
         if (!Auth::attempt($loginRequest->only(['login', 'password']))) {
-            return response()->json(['status' => 'invalid login and password']);
+            return response()->json(new ErrorResponse(
+                type: '/errors/invalid-login-or-password',
+                title: 'Неверный логин или пароль',
+                status: Response::HTTP_BAD_REQUEST,
+                detail: 'Попробуйте изменить параметры'
+            ));
         }
 
         $loginRequest->session()->regenerate();
 
-        return response()->json(['status' => 'ok']);
+        return response()->json();
     }
 
     public function test(Request $request): JsonResponse {
