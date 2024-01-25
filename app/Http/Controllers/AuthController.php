7ebfb98a03b5749b653ad8f9d3904b7027c29f48
proger0014 <iakov56@yandex.ru>
2024-01-25 11:19:36 +0600

    Добавил регистрацию

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Abstractions\RoleService;
+use App\Abstractions\UserService;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Responses\Error\ErrorResponse;
+use DateTimeImmutable;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
+use App\Dto\User\UserDto;
+
+use function Laravel\Prompts\password;
 
 class AuthController extends Controller
 {
+    public function __construct(
+        private readonly UserService $userService,
+        private readonly RoleService $roleService
+    ) { }
+
     public function login(LoginRequest $loginRequest): JsonResponse {
         if (!Auth::attempt($loginRequest->only(['login', 'password']))) {
             return response()->json(new ErrorResponse(
@@ -29,6 +41,27 @@ class AuthController extends Controller
 
     public function logout(Request $request): JsonResponse {
         Auth::guard('web')->logout();
+
+        return response()->json(data: null, status: Response::HTTP_NO_CONTENT);
+    }
+
+    public function register(RegisterRequest $registerRequest): JsonResponse {
+        $result = $this->userService->addNewUser(new UserDto(
+            id: null,
+            firstName: 'Не определено',
+            lastName: 'Не определено',
+            surname: 'Не определено',
+            login: $registerRequest->input('login'),
+            dateOfBirth: new DateTimeImmutable('2000-01-01'),
+            rating: null,
+            createdAt: null,
+            updatedAt: null
+        ), $registerRequest->input('password'));
+
+        if ($result->isError()) {
+            $error = $result->getError();
+            return response()->json($error, $error->status);
+        }
 
         return response()->json(data: null, status: Response::HTTP_NO_CONTENT);
     }
