d5525970ed635196eda5b67c5c59202b45414d21
proger0014 <iakov56@yandex.ru>
2024-01-26 00:27:03 +0600

    Добавил logout в сваггер; Некоторые фиксы

--- swagger/Controllers/AuthController.php
+++ swagger/Controllers/AuthController.php
@@ -8,24 +8,25 @@ use Swagger\Schemas\Error\ErrorResponse;
 use Swagger\Schemas\Error\ValidationErrorResponse;
 use Symfony\Component\HttpFoundation\Response;
 
+CONST AUTH_TAG = 'Auth';
 const AUTH = API . '/auth';
 
 interface AuthController {
     #[OA\Post(
         path: AUTH .'/login',
         operationId: 'login',
-        description: 'Устанавливает сессионную куку',
+        description: 'Устанавливает сессионную куку или обновляет сессию',
         summary: 'Вход в систему',
-        tags: ['Auth']
+        tags: [AUTH_TAG]
     )]
-    #[OA\Response(response: Response::HTTP_NO_CONTENT, description: 'No Content')]
+    #[OA\Response(response: Response::HTTP_NO_CONTENT, description: 'Успешно')]
     #[OA\Response(
         response: Response::HTTP_BAD_REQUEST,
         description: 'Ошибка',
         content: new OA\JsonContent(
             oneOf: [
                 new OA\Schema(ref: ErrorResponse::class),
-                new OA\Schema(ref: ValidationErrorResponse::class, description: 'Ошибка валидации')
+                new OA\Schema(ref: ValidationErrorResponse::class)
             ]
         )
     )]
@@ -33,4 +34,18 @@ interface AuthController {
         #[OA\RequestBody(
             content: new OA\JsonContent(ref: LoginRequest::class)
         )] $loginRequest);
+
+    #[OA\Post(
+        path: AUTH . '/logout',
+        operationId: 'logout',
+        summary: 'Выйти из системы',
+        tags: [AUTH_TAG]
+    )]
+    #[OA\Response(response: Response::HTTP_NO_CONTENT, description: 'Успешно')]
+    #[OA\Response(
+        response: Response::HTTP_UNAUTHORIZED,
+        description: 'Неавторизованный пользователь',
+        content: new OA\JsonContent(ref: ErrorResponse::class)
+    )]
+    function logout();
 }
