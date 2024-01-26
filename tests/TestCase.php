d03557698113dc93701fcc40d3be1f5447c6397b
proger0014 <iakov56@yandex.ru>
2024-01-26 15:55:06 +0600

    Определил данный seeder в TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Database\Seeders\Tests\DatabaseSeederDefault as TestDatabaseSeederDefault;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -9,4 +10,9 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
     protected bool $seed = true;
+
+    /**
+     * @var class-string
+     */
+    protected string $seeder = TestDatabaseSeederDefault::class;
 }
