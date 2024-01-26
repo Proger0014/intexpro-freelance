7b83f0b448fdbf24073e7dfecdd357fdf8487357
proger0014 <iakov56@yandex.ru>
2024-01-26 17:02:10 +0600

    Обновил phpunit конфигурацию

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -4,12 +4,14 @@ namespace Tests\Feature\Auth;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-class LoginTests extends TestCase
+class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
+    #[Test]
     public function login(): void
     {
         $this->assertTrue(true);
