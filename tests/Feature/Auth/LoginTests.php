f9e87be293b7cf007f2f26fabb33dc93a63c7e91
proger0014 <iakov56@yandex.ru>
2024-01-26 15:46:53 +0600

    Начало для интеграционного тестирования

---
+++ tests/Feature/Auth/LoginTests.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTests extends TestCase
+{
+    use RefreshDatabase;
+
+
+    public function login(): void
+    {
+    }
+}
