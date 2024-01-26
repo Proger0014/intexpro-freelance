232c7017d47b9685f07fc46600c2e7cd69d78e38
proger0014 <iakov56@yandex.ru>
2024-01-26 13:43:51 +0600

    Пофиксил ValidationErrorResponse

--- app/Http/Responses/Error/ValidationErrorResponse.php
+++ app/Http/Responses/Error/ValidationErrorResponse.php
@@ -7,15 +7,23 @@ use Symfony\Component\HttpFoundation\Response;
 final class ValidationErrorResponse {
     public readonly string $type;
     public readonly string $title;
+    public readonly int $status;
 
     /**
-     * @param array<string, string> $errors
+     * @var array<string, array<string>>
+     */
+    public readonly array $errors;
+
+    /**
+     * @param array<string, array<string>> $errors
      */
     public function __construct(
-        public readonly array $errors,
-        public readonly int $status = Response::HTTP_BAD_REQUEST
+        array $errors,
+        int $status = Response::HTTP_BAD_REQUEST
     ) {
         $this->type = '/errors/validation';
         $this->title = 'Ошибка валидации';
+        $this->status = $status;
+        $this->errors = $errors;
     }
 }
