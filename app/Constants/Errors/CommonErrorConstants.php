43af6ea1b7fad8ee5164bcc8edccd68d417cf01c
KirShet <kirshet2000@gmail.com>
2024-01-28 23:41:49 +0600

    Добавлен класс CommonErrorConstants для хранения констант с ResultError; Interbal server error

---
+++ app/Constants/Errors/CommonErrorConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants\Errors;
+
+class CommonErrorConstants
+{
+    public const TYPE = "/errors/internal-server-error";
+    public const TITLE = "Неизвестная ошибка";
+    public const DETAILS = "Произошла какая-то ошибка";
+}
