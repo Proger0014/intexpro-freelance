2efe94335e31a074127dc3be4aa6c7bf3e530f7b
KirShet <116618071+KirShet@users.noreply.github.com>
2024-01-29 12:07:56 +0600

    мелкие недочёты

--- app/Constants/Errors/CommonErrorConstants.php
+++ app/Constants/Errors/CommonErrorConstants.php
@@ -5,7 +5,8 @@ namespace App\Constants\Errors;
 class CommonErrorConstants
 {
     public const TYPE = '/errors';
+    
     public const TYPE_INTERNAL_SERVER = CommonErrorConstants::TYPE . '/internal-server-error';
-    public const TITLE = 'Неизвестная ошибка';
-    public const DETAILS = 'Произошла какая-то ошибка';
+    public const TITLE__INTERNAL_SERVER = 'Неизвестная ошибка';
+    public const DETAILS__INTERNAL_SERVER = 'Произошла какая-то ошибка';
 }
