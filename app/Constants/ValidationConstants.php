49cefcfda8b0090751baf0a58eaad9299978a6b3
KirShet <116618071+KirShet@users.noreply.github.com>
2024-01-29 12:33:41 +0600

    исправление значений констант

--- app/Constants/ValidationConstants.php
+++ app/Constants/ValidationConstants.php
@@ -4,7 +4,7 @@ namespace App\Constants;
 
 class ValidationConstants
 {
-    public const REQUIRED = ":attribute не указан";
-    public const MIN = ":attribute должен быть не меньше %d символов";
-    public const MAX = ":attribute должен быть не больше %d символов";
+    public const REQUIRED = ':attribute не указан';
+    public const MIN = ':attribute должен быть не меньше %d символов';
+    public const MAX = ':attribute должен быть не больше %d символов';
 }
