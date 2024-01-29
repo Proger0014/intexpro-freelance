a9ad1a8dfbb4c7298488c525372177d88b7d5a7a
KirShet <116618071+KirShet@users.noreply.github.com>
2024-01-29 11:30:13 +0600

    Объявлена константа TYPE_INTERNAL_SERVER, TYPE - базовый путь

--- app/Constants/Errors/CommonErrorConstants.php
+++ app/Constants/Errors/CommonErrorConstants.php
@@ -4,7 +4,8 @@ namespace App\Constants\Errors;
 
 class CommonErrorConstants
 {
-    public const TYPE = "/errors/internal-server-error";
-    public const TITLE = "Неизвестная ошибка";
-    public const DETAILS = "Произошла какая-то ошибка";
+    public const TYPE = '/errors';
+    public const TYPE_INTERNAL_SERVER = CommonErrorConstants::TYPE . '/internal-server-error';
+    public const TITLE = 'Неизвестная ошибка';
+    public const DETAILS = 'Произошла какая-то ошибка';
 }
