68bee6dcdcd8699752f6ebe71b0d102a66016da9
proger0014 <iakov56@yandex.ru>
2024-01-30 13:57:22 +0600

    Удалил не нужное, по причине внедрение готового решения

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -50,14 +49,5 @@ class User extends Authenticatable
     public function getAuthIdentifier()
     {
         return $this->login;
-    }
-
-    /**
-     * @return BelongsToMany<Role>
-     */
-    public function roles(): BelongsToMany {
-        return $this->belongsToMany(Role::class, 'users_roles', 'user_id', 'role_id')
-            ->withPivot('created_at')
-            ->using(UserRolePivot::class);
     }
 }
