3caee10ca1314be76392769b2672e759a8881d04
KirShet <kirshet2000@gmail.com>
2024-02-01 11:42:53 +0600

    Добавлен класс UsersErrorConstants с константами

---
+++ app/Constants/Errors/UsersErrorConstants.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Constants\Errors;
+
+use App\Constants\Errors\CommonErrorConstants;
+
+class UsersErrorConstants
+{
+    public const TYPE_EXIST = CommonErrorConstants::TYPE . '/exists';
+    public const TITLE_EXIST = 'Юзер с таким логином уже существует';
+    public const DETAIL_EXIST = 'Попробуйте изменить логин или войти в существующий аккаунт';
+
+    public const TYPE_NOT_FOUND = CommonErrorConstants::TYPE . '/not-found';
+
+    public const TITLE_NOT_FOUND_BY_ID = 'Юзер с таким id не найден';
+    public const DETAIL_NOT_FOUND = 'Попробуйте использовать верные параметры';
+
+    public const TITLE_NOT_FOUND_BY_LOGIN = 'Юзера с таким логином не существует';
+    public const DETAIL_NOT_FOUND_BY_LOGIN = UsersErrorConstants::DETAIL_NOT_FOUND . ' или создать нового юзера';
+}
+
