ec8949f8a0069c8637c925ce38b883fe29eccb4f
KirShet <kirshet2000@gmail.com>
2024-02-01 13:46:50 +0600

    Добавленны константы forbidden в  CommonErrorConstants

--- app/Constants/Errors/CommonErrorConstants.php
+++ app/Constants/Errors/CommonErrorConstants.php
@@ -8,6 +8,10 @@ class CommonErrorConstants
 
     public const TYPE_NOT_FOUND = CommonErrorConstants::TYPE . '/not-found';
     public const DETAIL_NOT_FOUND = 'Попробуйте использовать верные параметры';
+
+    public const TYPE_FORBIDDEN = CommonErrorConstants::TYPE . '/forbidden';
+    public const TITLE_FORBIDDEN = 'Недостаточно прав';
+    public const DETAIL_FORBIDDEN = 'Попробуйте обратиться к более вышестоящему для данного действия';
     
     public const TYPE_INTERNAL_SERVER = CommonErrorConstants::TYPE . '/internal-server-error';
     public const TITLE_INTERNAL_SERVER = 'Неизвестная ошибка';
