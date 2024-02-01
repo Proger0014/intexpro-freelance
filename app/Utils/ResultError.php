fc1742175923e351bd7e61e689500981ed2759bc
KirShet <kirshet2000@gmail.com>
2024-02-01 12:03:07 +0600

    Добавлены константы в ResultError

--- app/Utils/ResultError.php
+++ app/Utils/ResultError.php
@@ -7,9 +7,9 @@ use App\Constants\Errors\CommonErrorConstants;
 
 final class ResultError {
     public function __construct(
-        public readonly string $type = CommonErrorConstants::TYPE,
-        public readonly string $title = CommonErrorConstants::TITLE,
+        public readonly string $type = CommonErrorConstants::TYPE_INTERNAL_SERVER,
+        public readonly string $title = CommonErrorConstants::TITLE_INTERNAL_SERVER,
         public readonly int $status = Response::HTTP_INTERNAL_SERVER_ERROR,
-        public readonly string $detail = CommonErrorConstants::DETAILS
+        public readonly string $detail = CommonErrorConstants::DETAILS_INTERNAL_SERVER
     ) { }
 }
