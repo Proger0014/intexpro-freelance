19a0602332a6eebcf39d193f8e385a0e1feb6974
proger0014 <iakov56@yandex.ru>
2024-02-03 19:24:48 +0600

    Небольшой фикс для удобства разработки

--- client/src/config/routes.jsx
+++ client/src/config/routes.jsx
@@ -1,10 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
 import { HOME_ROUTE, HomePage } from "../pages";
 
-const routes = [
+const routes = createBrowserRouter([
   {
     path: HOME_ROUTE,
     element: <HomePage />
   }
-];
+]);
 
 export default routes;
--- client/src/providers/RoutingProvider.jsx
+++ client/src/providers/RoutingProvider.jsx
@@ -2,10 +2,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routes } from "../config";
 
 function RoutingProvider() {
-  const browserRouter = createBrowserRouter(routes);
-
   return (
-    <RouterProvider router={browserRouter} />
+    <RouterProvider router={routes} />
   )
 }
 
