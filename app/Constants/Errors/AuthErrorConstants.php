59917913404f29d0bbeeaa098496e3438a89cb58
KirShet <kirshet2000@gmail.com>
2024-02-06 10:36:58 +0600

    Перенё константы forbidden в AuthErrorConstants

--- app/Constants/Errors/AuthErrorConstants.php
+++ app/Constants/Errors/AuthErrorConstants.php
@@ -9,4 +9,8 @@ class AuthErrorConstants
     public const TYPE_INVALID_LOGIN_OR_PASSWORD = CommonErrorConstants::TYPE . '/invalid-login-or-password';
     public const TITLE_INVALID_LOGIN_OR_PASSWORD = 'Неверный логин или пароль';
     public const DETAIL_INVALID_LOGIN_OR_PASSWORD = 'Попробуйте изменить параметры';
+
+    public const TYPE_FORBIDDEN = CommonErrorConstants::TYPE . '/forbidden';
+    public const TITLE_FORBIDDEN = 'Недостаточно прав';
+    public const DETAIL_FORBIDDEN = 'Попробуйте обратиться к более вышестоящему для данного действия';
 }
